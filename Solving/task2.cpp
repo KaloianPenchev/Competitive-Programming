2c67839d836dd453476dbe9e8b9d2fc34f19735b
Kaloian Penchev <kaloanpencev8@gmail.com>
2025-04-02 09:13:56 +0300

    updated

--- Solving/task2.cpp
+++ Solving/task2.cpp
@@ -2,8 +2,11 @@
 
 using namespace std;
 
+#define ll long long
+
 const int MAXN = 2 * 1e5 + 5;
 const int MAXM = 3 * 1e5 + 5;
+const ll INF = INT_MAX;
 
 struct DSU {
     int par[MAXN], sz[MAXN];
@@ -33,14 +36,14 @@ struct DSU {
     }
 };
 
-struct Edge{
+struct Edge {
     int from;
     int to;
     int risk;
     int cost;
 };
 
-struct to_node{
+struct to_node {
     int to;
     int risk;
     int cost;
@@ -48,26 +51,91 @@ struct to_node{
 
 int n, m;
 vector<Edge> g1(MAXM);
+vector<vector<to_node>> g2(MAXN);
 vector<vector<to_node>> mst(MAXM);
 
+int max_risk; 
+int cost_sum;
 
-void add_edge(int& a, int &b, int& c, int& d){
-    mst[a].push_back({b, c, d});
-    mst[b].push_back({a, c, d});
+priority_queue<pair<int,int>> pq;
+vector<int> dist(MAXN, INF);
+
+void dijkstra(int node, int limit){ 
+    fill(dist.begin(), dist.begin() + n, INF);
+    while(!pq.empty()) pq.pop();
+
+    pq.push({0, node});
+    dist[node] = 0;
+
+    while(!pq.empty()){
+        int nd = pq.top().second;
+        int nd_dist = -pq.top().first;
+        pq.pop();
+
+        if(nd_dist > dist[nd]) continue;
+
+        for(auto& to : g2[nd]){
+            if(to.cost > limit) continue; 
+            int nxt = to.to;
+            int nxtDist = dist[nd] + to.cost;
+
+            if(nxtDist < dist[nxt]){
+                dist[nxt] = nxtDist;
+                pq.push({-nxtDist, nxt});
+            }
+        }
+    }
+}
+
+bool check(int x){
+    dijkstra(0, x);                
+    if(dist[n - 1] <= x) return 1;
+    return 0;
+}
+
+void binary_search(){
+    int lo = 0, hi = INT_MAX;
+    while(lo <= hi){
+        int mid = lo + (hi - lo) / 2; 
+        if(check(mid)){
+            cost_sum = mid;
+            hi = mid - 1;
+        }
+        else {
+            lo = mid + 1;
+        }
+    }
+}
+
+void dfs(int node, int par){                       
+    if(node + 1 == n){                             
+        return;                                    
+    }
+    for(auto& t : mst[node]){
+        if(t.to == par) continue;
+        max_risk = max(max_risk, t.risk); 
+        dfs(t.to, node);
+    }
+}
+
+void add_edge(vector<vector<to_node>>& v, int& a, int &b, int& c, int& d){
+    v[a].push_back({b, c, d});
+    v[b].push_back({a, c, d});
 }
 
 bool cmp(const Edge& e1, const Edge& e2){
+    if(e1.risk == e2.risk){
+        return e1.cost < e2.cost;
+    }
     return e1.risk < e2.risk;
 }
-
 
 void build_mst(){
     DSU d; d.init();
 
-    sort(g1.begin(), g1.end(), cmp);
+    sort(g1.begin(), g1.begin() + m, cmp);
 
     int counter = g1.size();
-
     for(auto& edge : g1){
         if(counter == 0) break;
         int from = edge.from;
@@ -77,20 +145,26 @@ void build_mst(){
 
         if(!d.connected(from, to)){
             d.union_sets(from, to);
-            add_edge(from, to, risk, cost);
+            add_edge(mst, from, to, risk, cost);
             counter--;
         }
     }
 }
 
-
 void solve(){
     cin >> n >> m;
-    
     for(int i = 0; i < m ; i++){
-        int a, b, c, d; cin >> a >> b >> c >> d; --a, --b;
-        g1.push_back({a, b, c, d});
+        int a, b, c, d; cin >> a >> b >> c >> d;
+        --a; --b;
+        g1[i] = {a, b, c, d};
+        add_edge(g2, a, b, c, d);
     }
+
+    build_mst();
+    dfs(0, -1);
+    binary_search();
+
+    cout << max_risk << " " << cost_sum << '\n';
 }
 
 void speed(){
